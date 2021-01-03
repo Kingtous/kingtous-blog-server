c366f0536f6baa2897fa063a50fc574c2bb0890b
Kingtous <kingtous@qq.com>
2021-01-03 21:45:00 +0800

    add application test

---
+++ src/test/kotlin/cn/kingtous/blogserver/BlogServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.kingtous.blogserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
