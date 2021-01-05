51878cbb5d63228a3a2232fc7714f43944bffa16
Kingtous <kingtous@qq.com>
2021-01-05 20:25:30 +0800

    add jar servlet

--- src/main/kotlin/cn/kingtous/blogserver/BlogServerApplication.kt
+++ src/main/kotlin/cn/kingtous/blogserver/BlogServerApplication.kt
@@ -2,9 +2,10 @@ package cn.kingtous.blogserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class BlogServerApplication
+class BlogServerApplication : SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
     runApplication<BlogServerApplication>(*args)
